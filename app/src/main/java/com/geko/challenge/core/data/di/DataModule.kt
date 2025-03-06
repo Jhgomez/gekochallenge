c053c81cafc6a2680eadeea20c6a65ccf80064f8
Juan Gomez <juan.e.g.f@outlook.com>
2025-03-06 17:28:26 -0600

    bugfix: use provides in data module

--- app/src/main/java/com/geko/challenge/core/data/di/DataModule.kt
+++ app/src/main/java/com/geko/challenge/core/data/di/DataModule.kt
@@ -1,17 +1,25 @@
 package com.geko.challenge.core.data.di
 
+import com.geko.challenge.core.common.di.IoDispatcher
 import com.geko.challenge.core.data.repository.AuthenticationRepository
-import dagger.Binds
+import com.geko.challenge.core.database.dao.UserDao
+import com.geko.challenge.core.datastore.AppDatastore
+import com.geko.challenge.core.network.retrofit.RetrofitNetworkApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DataModule {
+internal object DataModule {
 
-    @Binds
-    internal abstract fun bindsTopicRepository(
-        topicsRepository: AuthenticationRepository
-    ): AuthenticationRepository
+    @Provides
+    fun bindsAuthRepository(
+        store: AppDatastore,
+        apiService: RetrofitNetworkApi,
+        userDao: UserDao,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): AuthenticationRepository = AuthenticationRepository(store, apiService, userDao, ioDispatcher)
 }
