deeef3f8481f044a60ed066bf04e0350b8115f62
Juan Gomez <juan.e.g.f@outlook.com>
2025-03-06 12:00:45 -0600

    chore: add table name

--- app/src/main/java/com/geko/challenge/core/database/model/UserEntity.kt
+++ app/src/main/java/com/geko/challenge/core/database/model/UserEntity.kt
@@ -4,7 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    tableName = "user",
+)
 data class UserEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "name") val name: String
