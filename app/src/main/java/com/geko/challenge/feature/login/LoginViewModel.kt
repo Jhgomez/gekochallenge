b239309f6058ded5477d7e1ad8ffd2e256ceaa29
Juan Gomez <juan.e.g.f@outlook.com>
2025-03-08 19:12:23 -0600

    chore: remove unncessary success state params

--- app/src/main/java/com/geko/challenge/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/geko/challenge/feature/login/LoginViewModel.kt
@@ -2,56 +2,27 @@ package com.applaudo.androidchallenge01.ui.screen.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.geko.challenge.core.data.repository.AuthenticationRepository
 import com.geko.challenge.core.ui.LoginLogicUiEvent
 import com.geko.challenge.core.ui.LoginUiState
 import com.geko.challenge.domain.LoginUseCase
 import com.geko.challenge.domain.common.UseCaseResult
-//import com.applaudo.androidchallenge01.R
-//import com.applaudo.androidchallenge01.util.Constants.AIRING_TODAY
-//import com.applaudo.androidchallenge01.util.Constants.ON_THE_AIR
-//import com.applaudo.androidchallenge01.util.Constants.POPULAR
-//import com.applaudo.androidchallenge01.util.Constants.TOP_RATED
-//import com.applaudo.domain.interactor.GetTvShowsUseCase
-//import com.applaudo.domain.model.Category
-//import com.applaudo.domain.model.TvShow
-//import com.applaudo.domain.util.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase,
-    private val authenticationRepository: AuthenticationRepository
+    private val loginUseCase: LoginUseCase
 ) : ViewModel() {
-
-    private val emailError = MutableStateFlow<Boolean>(false)
-    private val passwordError = MutableStateFlow<Boolean>(false)
 
     private val _uiEvent: MutableStateFlow<LoginLogicUiEvent?> = MutableStateFlow(null)
     val uiEvent: StateFlow<LoginLogicUiEvent?> = _uiEvent.asStateFlow()
 
-    val uiState: StateFlow<LoginUiState> =
-        combine(
-            emailError,
-            passwordError
-        ) { emailE, passwordE ->
-            LoginUiState.Success(
-                emailError = emailE,
-                passwordError = passwordE
-            )
-        }.stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5_000),
-                initialValue = LoginUiState.Loading,
-            )
+    private val _uiState: MutableStateFlow<LoginUiState> = MutableStateFlow(LoginUiState.Success)
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
     fun login(email: String, password: String) {
         viewModelScope.launch {
@@ -70,7 +41,8 @@ class LoginViewModel @Inject constructor(
                     )
                 }
                 is UseCaseResult.Succeed -> {
-                    _uiEvent.value = LoginLogicUiEvent.NavigateToUserScreen(result.data.name)
+                    // no need to anything as if user authenticates then app state changes,
+                    // it is not this view state that changes
                 }
             }
 
