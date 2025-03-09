7f09cee0476ea39b8661c261b01e60666e09b3fd
Juan Gomez <juan.e.g.f@outlook.com>
2025-03-08 19:47:01 -0600

    chore: add sigup viewmodel

---
+++ app/src/main/java/com/geko/challenge/feature/signup/SignUpViewModel.kt
@@ -0,0 +1,51 @@
+package com.applaudo.androidchallenge01.ui.screen.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.geko.challenge.core.ui.SignUpLogicUiEvent
+import com.geko.challenge.core.ui.SignUpUiState
+import com.geko.challenge.domain.SignUpUseCase
+import com.geko.challenge.domain.common.UseCaseResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val signUpUseCase: SignUpUseCase
+) : ViewModel() {
+
+    val _uiEvent: MutableStateFlow<SignUpLogicUiEvent?> = MutableStateFlow(null)
+    val uiEvent: StateFlow<SignUpLogicUiEvent?> = _uiEvent.asStateFlow()
+
+    val _UiState: MutableStateFlow<SignUpUiState> = MutableStateFlow(SignUpUiState.Success())
+    val uiState: StateFlow<SignUpUiState> = _UiState.asStateFlow()
+
+    fun register(
+        firstName: String,
+        lastName: String,
+        email: String,
+        password: String
+    ) {
+        viewModelScope.launch {
+            var state = _UiState.value
+            if (state is SignUpUiState.Success) {
+                _UiState.value = state.copy(isLoading = true)
+
+                when(val result = signUpUseCase(firstName, lastName, email, password)) {
+                    is UseCaseResult.Failed -> {
+                        _UiState.value = state.copy(isLoading = false)
+                    }
+                    is UseCaseResult.Succeed -> {
+                        _UiState.value = state.copy(isLoading = false)
+                        // no need to navigate as if user authenticates then app state changes,
+                        // meaning it is not this view's state that changes
+                    }
+                }
+            }
+        }
+    }
+}
